7a445861e0ea5293642f2fdaa1dcedad12575c2f
Arkadiusz Dobosz <doboszarkadiusz01@gmail.com>
2017-08-16 19:28:08 +0200

    Send method added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,62 +6,64 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 use AppBundle\Form\ContactForm;
 
-class DefaultController extends Controller
+class DefaultController extends FOSRestController
 {
     private $email = 'tasktest88@gmail.com';
     
     /**
-     * @Route("/", name="homepage")
+     * @Rest\Post("/send")
      */
-    public function indexAction(Request $request, \Swift_Mailer $mailer)
-    {
+    public function sendAction(Request $request, \Swift_Mailer $mailer) {
         
-        $form = $this->createForm(ContactForm::class);
+        $form = $this->createForm(ContactForm::class, null, [
+            'csrf_protection' => false,        
+        ]);
         
-        if($request->isMethod('POST')) {
-            $form->handleRequest($request);
-            
-            if($form->isValid() && $form->isSubmitted()){
-                $data = $request->request->all();
-                
-                $session = $this->get('session')->getFlashBag();
-                if($this->sendMail($data, $mailer)) {
-                    $session->add('success', 'Wiadomość została wysłana');
-                    return $this->redirectToRoute('homepage');
-                } else {
-                    $session->add('danger', 'Błąd podczas wysyłania wiadomości');
-                }                  
-            }            
+        $form->submit($request->request->all());
+        
+        if(!$form->isValid()) {
+            return $form;
         }
         
-        return $this->render('AppBundle:Default:show.html.twig', array(
-            'form' => $form->createView()
-        ));
+        $data = $request->request->all();
+        
+        if($this->sendMail($data, $mailer)) {
+            return new View("Email has sent", Response::HTTP_OK);
+        }
     }
     
-    private function sendMail(array $data, \Swift_Mailer $mailer) {
-                $from = $data['contact_form']['email'];
-                $content = $data['contact_form']['content'];
-                $sender = $data['contact_form']['sender'];
-                $title = $data['contact_form']['title'];
-                
-                $message = (new \Swift_Message())
-                        ->setSubject($title)
-                        ->setFrom(['botblackd988@gmail.com' => 'Arek Dobosz'])
-                        ->setReplyTo($from)
-                        ->setTo($this->email)
-                        ->setBody(
-                            $this->renderView(
-                                'AppBundle:Emails:send_email.html.twig',
-                                array(
-                                    'content' => $content,
-                                    'sender' => $sender
-                                )
-                            ),
-                            'text/html'
-                        );
+    private function sendMail(array $data, $mailer) {
+        
+        $from = $data['email'];
+        $content = $data['content'];
+        $sender = $data['sender'];
+        $title = $data['title'];
+
+        $message = (new \Swift_Message())
+                ->setSubject($title)
+                ->setFrom(['botblackd988@gmail.com' => 'Arek Dobosz'])
+                ->setReplyTo($from)
+                ->setTo($this->email)
+                ->setBody(
+                    $this->renderView(
+                        'AppBundle:Emails:send_email.html.twig',
+                        array(
+                            'content' => $content,
+                            'sender' => $sender
+                        )
+                    ),
+                    'text/html'
+                );
             
         return $mailer->send($message);
     }
